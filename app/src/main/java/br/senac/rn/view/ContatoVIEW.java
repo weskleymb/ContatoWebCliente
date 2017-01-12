74cab221f429259bcbb7254462e189b0ee8cf275
Weskley Bezerra <weskleymb@hotmail.com>
2017-01-12 13:44:12 -0300

    Initial commit

--- app/src/main/java/br/senac/rn/view/ContatoVIEW.java
+++ app/src/main/java/br/senac/rn/view/ContatoVIEW.java
@@ -1,13 +1,15 @@
 package br.senac.rn.view;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import br.senac.rn.dao.ContatoDAO;
 import br.senac.rn.model.Contato;
 
@@ -16,6 +18,7 @@ public class ContatoVIEW extends Activity {
     private EditText etId;
     private Button btBuscar;
     private TextView tvNome, tvFone, tvEmail;
+    private ProgressDialog load;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,26 +40,54 @@ public class ContatoVIEW extends Activity {
         btBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-                Contato contato = buscar(Integer.parseInt(etId.getText().toString()));
-                if (contato != null) {
-                    tvNome.setText(contato.getNome());
-                    tvFone.setText(contato.getFone());
-                    tvEmail.setText(contato.getEmail());
-                }
-                limpar();
+                buscar(Integer.parseInt(etId.getText().toString()));
             }
         });
     }
 
-    private Contato buscar(int id) {
-        return new ContatoDAO().buscarId(id);
+    private void buscar(int id) {
+        new TarefaDownload().execute(id);
     }
 
     private void limpar() {
         etId.setText("");
         etId.requestFocus();
+    }
+
+    private class TarefaDownload extends AsyncTask<Integer, Integer, Contato> {
+
+        @Override
+        protected void onPreExecute(){
+            load = ProgressDialog.show(ContatoVIEW.this, "Por favor aguarde...", "Buscando dados...");
+        }
+
+        @Override
+        protected Contato doInBackground(Integer... ints) {
+            return new ContatoDAO().buscarId(ints[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Contato contato) {
+            load.dismiss();
+            if (contato != null) {
+                tvNome.setText(contato.getNome());
+                tvFone.setText(contato.getFone());
+                tvEmail.setText(contato.getEmail());
+            } else {
+                tvNome.setText("");
+                tvFone.setText("");
+                tvEmail.setText("");
+
+                Context contexto = getApplicationContext();
+                String texto = "Contato não encontrado";
+                int duracao = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(contexto, texto, duracao);
+                toast.show();
+            }
+            limpar();
+        }
+
     }
 
 }
